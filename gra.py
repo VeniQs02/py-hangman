343f221cbc736d181e2be44a04aaced44db27245
Jakub <jakub.kangowski7@vp.pl>
2022-05-05 16:56:05 +0200

    life checki i zdrapka do porównywania

--- gra.py
+++ gra.py
@@ -6,32 +6,33 @@ def initial_word_formation():
             print("Podaj poprawna wartosc!")
             continue
         slowo = list(slowo)
-        #print(slowo)
+        # print(slowo)
         return slowo
+
+def life_check(slowo):
+    zdrapka = []
+    dlugosc_slowa = len(slowo)
+    for i in range(dlugosc_slowa):
+        zdrapka.append('_')
+    print(' '.join(zdrapka))
+    zycia = 10
+    while zycia > 0:
+        literka = input("Podaj literkę: ").upper()
+        if (literka == ''):
+            print("Podaj poprawna wartosc!")
+            continue
+        print(f"Literka to: {literka}")
+        zdrapka_check = zdrapka
+        for i in range(dlugosc_slowa):
+            if (slowo[i] == literka):
+                zdrapka[i] = literka
+            if (zdrapka_check == zdrapka):
+                zycia -= 1
+        print(' '.join(zdrapka))
 
 def gameplay():
     while True:
         slowo = initial_word_formation()
-        zdrapka = []
-        dlugosc_slowa = len(slowo)
-        for i in range(dlugosc_slowa):
-            zdrapka.append('_')
-        print(' '.join(zdrapka))
-        while True:
-            literka = input("Podaj literkę: ").upper()
-            if(literka == ''):
-                print("Podaj poprawna wartosc!")
-                continue
-            print(f"Literka to: {literka}")
-            for i in range(dlugosc_slowa):
-                if(slowo[i] == literka):
-                    zdrapka[i] = literka
-            print(' '.join(zdrapka))
-
-
-
-
-
-
+        life_check(slowo)
 
 gameplay()
