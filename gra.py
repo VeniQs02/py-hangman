8cc74ee82675407fdee5bdecc9449bbc8e6f2700
Jakub <jakub.kangowski7@vp.pl>
2022-05-07 19:45:35 +0200

    wiem już co było źle i naprawiłem, gra funkcjonalna ale wymaga szlifu

--- gra.py
+++ gra.py
@@ -1,18 +1,18 @@
 def initial_word_formation():
     while True:
         print("\n\nPodaj słowo które będzie trzeba odgadnąć: \n> ", end='')
-        slowo = input().upper()
+        slowo = input()
         if slowo == '':
             print("Podaj poprawna wartosc!")
             continue
-        slowo = list(slowo)
-        # print(slowo)
+        slowo = list(slowo.upper())
+        print(slowo)
         return slowo
 
 def drawing_hang_station(zycia):
     match zycia:
         case 10:
-            print('\n\n\n\n\n\n')
+            pass
         case 9:
             print('\n\n\n\n\n\n ______')
         case 8:
@@ -33,6 +33,8 @@ def drawing_hang_station(zycia):
             print(' _____\n|    |\n|    0\n|   /|\ \n|  _/\n|\n|______')
         case 0:
             print(' _____\n|    |\n|    0\n|   /|\ \n|  _/ \_ \n|\n|______')
+        case _:
+            print('')
 
 def life_check(slowo):
     zdrapka = []
@@ -46,22 +48,27 @@ def life_check(slowo):
         if (literka == ''):
             print("Podaj poprawna wartosc!")
             continue
-        print(f"Literka to: {literka}")
-        zdrapka_check = zdrapka
+        zdrapka_check = zdrapka.copy()
+
         for i in range(dlugosc_slowa):
             if (slowo[i] == literka):
                 zdrapka[i] = literka
-            if (zdrapka_check == zdrapka):
-                zycia -= 1
+        if (zdrapka_check == zdrapka):
+            zycia -= 1
+        if(slowo == zdrapka):
+            print("Wygrałeś!")
+            break
         drawing_hang_station(zycia)
         print(' '.join(zdrapka))
+        if(zycia == 0):
+            print('Przegrałeś!')
 
 
 def gameplay():
     while True:
         slowo = initial_word_formation()
         life_check(slowo)
+        break
 
-for i in range(11):
-    drawing_hang_station(10-i)
-#gameplay()
+gameplay()
+#3001447
